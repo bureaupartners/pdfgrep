d8ee8d836e4cf05bc92b333963f66d3b6ad39830
Mark Hameetman <mark@bureau.partners>
2020-04-04 21:42:36 +0200

    fix path in test suite

--- tests/ExtractPdfTest.php
+++ tests/ExtractPdfTest.php
@@ -7,21 +7,21 @@
  * @copyright Copyright (c) 2019, GRIVOS Holding B.V.
  */
 
-declare(strict_types=1);
-use PHPUnit\Framework\TestCase;
+declare (strict_types = 1);
 use BureauPartners\pdfgrep\pdfgrep;
+use PHPUnit\Framework\TestCase;
 
 final class ExtractPdfTest extends TestCase
 {
     public function testCanExtractWord()
     {
-        $pdfgrep = new pdfgrep('/var/www/html/tests/pdf/Document.pdf', 'id');
+        $pdfgrep = new pdfgrep(dirname(__FILE__) . '/pdf/Document.pdf', 'id');
         $this->assertCount(7, $pdfgrep->getMatches());
     }
 
     public function testCanGetPageNumbers()
     {
-        $pdfgrep = new pdfgrep('/var/www/html/tests/pdf/Document.pdf', 'id');
+        $pdfgrep = new pdfgrep(dirname(__FILE__) . '/pdf/Document.pdf', 'id');
         $this->assertCount(1, $pdfgrep->getPageNumbers());
     }
 }
